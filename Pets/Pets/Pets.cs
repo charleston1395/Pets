db76318dc5bc2dcf7800dd695c94155d8389d40b
Charleston Gilliland <clstlmusic@gmail.com>
2019-09-18 10:21:34 -0500

    pets project

---
+++ Pets/Pets/Pets.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pets
+{
+    public class Pet
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public int Legs { get; set; }
+        public string Breed { get; set; }
+        public Pet(int age, string name, int legs, string breed )
+        {
+            Age = age;
+            Name = name;
+            Legs = legs;
+            Breed = breed;
+
+        }
+
+        public Pet()
+        {
+
+        }
+        
+        
+        }
+    }
+
---
+++ Pets/Pets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet dog1 = new Pet();
+            dog1.Age = 3;
+            dog1.Name = "Ralph"; 
+            dog1.Legs = 3;
+            dog1.Breed = "Husky";
+
+            var cat = new Pet(6, "whiskers", 4, "Short-Hair");
+
+            List<Pet> myPets = new List<Pet>();
+            myPets.Add(dog1);
+            myPets.Add(cat);
+
+            foreach(var animal in myPets)
+            {
+                Console.WriteLine($"Pet Name;{animal.Name}, Age of pet: {animal.Age}");
+            }
+
+        }
+    }
+}
